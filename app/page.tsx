e397123ef6c529eca7af8e1b83099ea25a71f5ce
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-09 01:26:36 +0530

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
 const page = async()=>{
-  const {data} = await  axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/sample`);
-  return <div>axios data: {data.message}</div>
+  // const {data} = await  axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/sample`);
+  return <div>axios data: {"data.message"}</div>
 }
 
 export default page;
