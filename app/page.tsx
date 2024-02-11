878eb00b0ec0a0385c8565a0a370e5321ceeec35
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-11 15:22:25 +0530

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const page = async()=>{
-  const {data} = await  axios.get(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/sample`);
+  const {data} = await  axios.get(`/api/sample`);
   return <div>axios data: {data.message}</div>
 }
 
