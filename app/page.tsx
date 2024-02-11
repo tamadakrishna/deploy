2e85de6227bd01276b4b1a8f59e1f23f6dd883ca
tamadakrishna <tamadakrishnaa@gmail.com>
2024-02-11 16:02:47 +0530

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const page = async()=>{
-  const {data} = await  axios.get(`/api/sample`);
-  return <div>axios data: {data.message}</div>
+  // const {data} = await  axios.get(`${process.env.}/api/sample`);
+  return(
+    <ul>
+      <li>Vercel Env: {process.env.VERCEL_ENV}</li>
+      <li>Environment Var: {process.env.ENV_VAR}</li>
+    </ul>
+  )
 }
 
 export default page;
